13a7711126ce91cac0b393fdd83013f84810eff9
Roland Tanglao <roland@rolandtanglao.com>
2020-06-14 00:38:40 -0700

    add forum support report for may 2020

---
+++ get-question-count-all-products.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+grep ",firefox," $1 | wc -l 
+grep ",thunderbird," $1 | wc -l 
+grep ",mobile," $1 | wc -l 
+grep ",Focus-ios," $1 | wc -l 
+grep ",focus-firefox," $1 | wc -l 
+grep ",firefox-lockwise," $1 | wc -l 
+grep ",firefox-preview," $1 | wc -l 
+grep ",ios," $1 | wc -l 
+grep ",firefox-lite," $1 | wc -l 
+grep ",firefox-fire-tv," $1 | wc -l 
+grep ",firefox-amazon-devices," $1 | wc -l 
+grep ",firefox-reality," $1 | wc -l 
+grep ",firefox-private-network," $1 | wc -l 
+grep ",firefox-private-network-vpn," $1 | wc -l 
+grep ",firefox-enterprise," $1 | wc -l 
+grep ",firefox-os," $1 | wc -l 
